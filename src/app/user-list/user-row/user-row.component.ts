e1ec5c422b51dd174bcce773a19c879d23bf8b78
AntalBodnarSSI <antal.bodnar@surveysampling.com>
2016-10-28 17:00:13 +0200

    use the actual userdata in user-row component

--- src/app/user-list/user-row/user-row.component.ts
+++ src/app/user-list/user-row/user-row.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'user-row',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class UserRowComponent {
+
+  @Input('user-object') userObj = null;
 
 }
