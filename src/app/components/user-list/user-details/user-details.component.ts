fa6c9336762e81d0c7f1a6cda21a5cdbee0c744c
AntalBodnarSSI <antal.bodnar@surveysampling.com>
2016-10-30 12:04:25 +0100

    add user-details component

---
+++ src/app/components/user-list/user-details/user-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
+import { UserModel } from "../../../models/user.model";
+import { UserService } from "../../../services/user.service";
+
+@Component({
+  selector: 'user-details',
+  templateUrl: 'user-details.component.html',
+  styleUrls: ['user-details.component.css'],
+  providers: [
+    UserService
+  ]
+})
+
+export class UserDetailsComponent implements OnInit, OnDestroy {
+
+  id: number = null;
+  idSubscription: Subscription = null;
+  userObj: UserModel = null;
+
+  constructor(private _route: ActivatedRoute, private _userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.idSubscription = this._route.params.subscribe(params => {
+      this.id = params['id'];
+      this._userService.getUserById(this.id).subscribe(result => this.userObj = result);
+    });
+  }
+
+  ngOnDestroy() {
+    this.idSubscription.unsubscribe();
+  }
+
+
+}
